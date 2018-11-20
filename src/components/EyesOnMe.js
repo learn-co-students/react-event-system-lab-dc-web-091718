d89b535c9354ff7f36ec5223a2f6b7485b9274b9
Flatiron School <flatironschool@Flatirons-MacBook-Air-2.local>
2018-11-20 10:52:53 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+export default class EyesOnMe extends Component {
+  focusFn = () => console.log('Good!')
+  blurFn = () => console.log('Hey! Eyes on me!')
+  render() {
+
+
+    return (
+      <div>
+        <button onFocus={this.focusFn} onBlur={this.blurFn} />
+      </div>
+    );
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+  export default class Keypad extends Component {
+    render() {
+
+      const keyUpFn = () => console.log('Entering password...')
+
+      return (
+        <div>
+          <input type="password"
+            onKeyUp={keyUpFn} />
+        </div>
+      );
+    }
+  }
